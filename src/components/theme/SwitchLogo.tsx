aeed5f44b5463e36bd9b2daf39b28b2c0027e6ec
Elcho911 <98739225+Elkhan2003@users.noreply.github.com>
2023-04-16 18:22:43 +0600

    Update SwitchLogo.tsx

--- src/components/theme/SwitchLogo.tsx
+++ src/components/theme/SwitchLogo.tsx
@@ -1,50 +1,22 @@
-import React, { FC, useState, useEffect } from "react";
+import React, { FC } from "react";
 import Image from "next/image";
-import logoDevX from "@/assets/logo-dark.png";
-import logoDevX_dark from "@/assets/logo.png";
+import logoDark from "@/assets/logo-dark.png";
+import logoLight from "@/assets/logo.png";
 import { useTheme } from "next-themes";
 
 interface SwitchLogoProps {
-	className?: any;
+	className?: string;
 }
 
 const SwitchLogo: FC<SwitchLogoProps> = ({ className }) => {
-	const [mounted, setMounted]: any = useState(false);
-
-	useEffect(() => {
-		setMounted(true);
-	}, []);
-
-	if (!mounted) {
-		return null;
-	}
-
-	const logo: any = logoDevX;
-	const logo_dark: any = logoDevX_dark;
-
 	const { resolvedTheme }: any = useTheme();
-	let src;
-
-	switch (resolvedTheme) {
-		case "light":
-			src = logo;
-			break;
-		case "dark":
-			src = logo_dark;
-			break;
-		default:
-			src = null;
-			break;
-	}
+	const logo: any = resolvedTheme === "light" ? logoDark : logoLight;
 
 	return (
 		<>
-			{src ? (
-				<Image className={className} src={src} alt="logo" />
-			) : (
-				<p>Unable to determine theme.</p>
-			)}
+			<Image className={className} src={logo} alt="logo" />
 		</>
 	);
 };
+
 export default SwitchLogo;
