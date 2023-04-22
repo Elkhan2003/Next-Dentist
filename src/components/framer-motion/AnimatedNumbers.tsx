bac28f7afbcd66b5f2d24811d24567b01896f8cb
Elcho911 <98739225+Elkhan2003@users.noreply.github.com>
2023-04-23 02:33:42 +0600

    Update AnimatedNumbers.tsx

--- src/components/framer-motion/AnimatedNumbers.tsx
+++ src/components/framer-motion/AnimatedNumbers.tsx
@@ -9,7 +9,7 @@ const AnimatedNumbers: FC<AnimatedNumbersProps> = ({ value }) => {
 	const ref: any = useRef(null);
 
 	const motionValue: any = useMotionValue(0);
-	const springValue: any = useSpring(motionValue, { duration: 5000 });
+	const springValue: any = useSpring(motionValue, { duration: 7000 });
 	const isView: boolean = useInView(ref);
 
 	useEffect(() => {
