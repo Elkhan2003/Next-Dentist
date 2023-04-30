cdcf86da25c5c701bd230517b362ff34852d1d08
Elcho911 <98739225+Elkhan2003@users.noreply.github.com>
2023-05-01 00:46:03 +0600

    Added slider for Certificates

--- src/components/tabs/tab__pages/TabСertificate.tsx
+++ src/components/tabs/tab__pages/TabСertificate.tsx
@@ -1,20 +1,80 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode, Navigation, Thumbs } from "swiper";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
+import scss from "./TabPages.module.scss";
+import Image from "next/image";
+import pic from "@/assets/DnI9rquWsAAgfKx-min.png";
 
-interface TabProps {
-	title?: any;
-	text?: any;
+interface imagesProps {
+	id: number;
+	img: any;
 }
 
-const TabСertificate: FC<TabProps> = ({ title, text }) => {
+const images: imagesProps[] = [
+	{
+		id: 1,
+		img: pic
+	},
+	{
+		id: 2,
+		img: pic
+	},
+	{
+		id: 3,
+		img: pic
+	},
+	{
+		id: 4,
+		img: pic
+	},
+	{
+		id: 5,
+		img: pic
+	}
+];
+
+const TabСertificate: FC = () => {
+	const [thumbsSwiper, setThumbsSwiper] = useState<any>(null);
+
 	return (
 		<>
-			<div className={title}>Сertificate Content</div>
-			<p className={text}>
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi
-				excepturi ducimus sequi dignissimos expedita tempore omnis quos cum,
-				possimus, aspernatur esse nihil commodi est maiores dolorum rem iusto
-				atque, beatae voluptas sit eligendi architecto.
-			</p>
+			<div className={scss.Certificate__container}>
+				<Swiper
+					spaceBetween={10}
+					navigation={true}
+					thumbs={{
+						swiper:
+							thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null
+					}}
+					modules={[FreeMode, Navigation, Thumbs]}
+					className={scss.mySwiper1}
+				>
+					{images.map((items) => (
+						<SwiperSlide key={items.id}>
+							<Image className={scss.image} src={items.img} alt="Сertificate" />
+						</SwiperSlide>
+					))}
+				</Swiper>
+				<Swiper
+					onSwiper={setThumbsSwiper}
+					spaceBetween={10}
+					slidesPerView={4}
+					freeMode={true}
+					watchSlidesProgress={true}
+					modules={[FreeMode, Navigation, Thumbs]}
+					className={scss.mySwiper2}
+				>
+					{images.map((items) => (
+						<SwiperSlide key={items.id}>
+							<Image className={scss.image} src={items.img} alt="Сertificate" />
+						</SwiperSlide>
+					))}
+				</Swiper>
+			</div>
 		</>
 	);
 };
